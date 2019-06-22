e61bc7fc5fedaa9d4ae5f3efecce77077c85876c
RedEyedMars <greg_estouffey@hotmail.com>
2019-06-22 13:43:30 -0400

    commit: pushing resources from sql, start work

--- src/databasing/setup.go
+++ src/databasing/setup.go
@@ -9,7 +9,18 @@ import (
 	"log"
 )
 
+var dbQueries map[string]string
+var dbQueryArgumentLength map[string]int
+var ResourceRequests chan *DBResourceResponse
+
 func Setup() {
+	dbQueries = make(map[string]string)
+	dbQueryArgumentLength = make(map[string]int)
+	ResourceRequests = make(chan *DBResourceResponse)
+	Events.FuncEvent("databasing.SetupResources", SetupResources())
+}
+
+func Start() {
 	defer common_chat.MainEnd()
 	Logger.Verbose <- Logger.Msg{"Setting up database..."}
 	Events.FuncEvent("databasing.StartDatabase", func() {
@@ -26,7 +37,32 @@ func Setup() {
 			if err = db.Ping(); err != nil {
 				log.Fatal(err)
 			}
+			Events.GoFuncEvent("databasing.StartMessageListening", func() { StartMessageListening(db) })
 			Logger.Verbose <- Logger.Msg{"Closing database..."}
 		}
 	})
+}
+func StartMessageListening(db *sql.DB) {
+	for {
+		select {
+		case request := <-ResourceRequests:
+			if rows, err := db.Query(request.Query()); err != nil {
+				Logger.Error <- Logger.ErrMsg{err, "StartMessageListening.ResourceRequest.Query"}
+			} else {
+				for rows.Next() {
+					var (
+						name   string
+						source string
+						abv    string
+					)
+					if err := rows.Scan(&name, &source, &abv); err != nil {
+						Logger.Error <- Logger.ErrMsg{err, "StartMessageListening.ResourceRequest.Scan"}
+					}
+					request.Resources <- Resource{name, source, abv}
+				}
+			}
+		default:
+			return
+		}
+	}
 }
