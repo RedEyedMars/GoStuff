6a40410fcc0c2f8fc7be86d78c724817470ac027
RedEyedMars <greg_estouffey@hotmail.com>
2019-10-14 11:23:09 -0400

    push the width all the way.

--- src/www/js/channels.js
+++ src/www/js/channels.js
@@ -5,14 +5,13 @@ function appendChannel(channel_name){
   item.innerHTML = channel_name;
   item.className = "channel";
   channels.appendChild(item);
-  return item;
 };
 
 commands["channel_names"] = function(result){
     var messages = result.split(';;');
     for (var i = 0; i < messages.length; i++) {
-      channels = appendChannel(messages[i]);
-      channels = appendChannel(messages[i]);
+      appendChannel(messages[i]);
+      appendChannel(messages[i]);
     }
 
 };
