2f4b9c63c8920f56ed6e3147f12eff12ef8e5f0f
RedEyedMars <greg_estouffey@hotmail.com>
2019-10-14 10:59:35 -0400

    Display channel names.

--- src/www/js/channels.js
+++ src/www/js/channels.js
@@ -1,8 +1,16 @@
 
 
+function appendChannel(channel_name){
+  var item = document.createElement("div");
+  item.innerHTML = inner.substring(indexOfColon+2,inner.length);
+  item.title = chat_user;
+  item.className = "channel";
+  channels.appendChild(item);
+};
+
 commands["channel_names"] = function(result){
     var messages = result.split(';;');
     for (var i = 0; i < messages.length; i++) {
-      appendLog(createTextLinks_(messages[i]));
+      appendChannel(messages[i]);
     }
 };
