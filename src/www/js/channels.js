175d374764f3934d01893ffff44493febcb0a67a
RedEyedMars <greg_estouffey@hotmail.com>
2019-10-14 11:19:15 -0400

    seeing what it looks like with 2 channels

--- src/www/js/channels.js
+++ src/www/js/channels.js
@@ -11,5 +11,6 @@ commands["channel_names"] = function(result){
     var messages = result.split(';;');
     for (var i = 0; i < messages.length; i++) {
       appendChannel(messages[i]);
+        appendChannel(messages[i]);
     }
 };
