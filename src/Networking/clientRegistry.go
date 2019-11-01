f72bd062b503bd6553b2ebc195b851e3c379142f
RedEyedMars <greg_estouffey@hotmail.com>
2019-10-31 21:49:53 -0400

    some more logging

--- src/Networking/clientRegistry.go
+++ src/Networking/clientRegistry.go
@@ -33,8 +33,9 @@ func (h *ClientRegistry) run_registry() {
 			Events.FuncEvent("ClientRegistry.Unregister", func() {
 				if _, ok := h.clients[client]; ok {
 					delete(h.clients, client)
-					for _, chl := range client.channels {
-						Events.FuncEvent("ClientRegistry.RemoveClient:"+client.name+" from "+chl.Channel.Name, func() {
+					Events.FuncEvent("ClientRegistry.RemoveClient:"+string(len(client.channels)), func() {})
+					for chlname, chl := range client.channels {
+						Events.FuncEvent("ClientRegistry.RemoveClient:"+chlname+" from "+chl.Channel.Name, func() {
 							chl.Channel.RemoveClient <- client.send
 						})
 					}
